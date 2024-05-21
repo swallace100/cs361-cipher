2e75e1ac07af8b93eefcb6fadd23215a87ad69b0
Steven Wallace <swallace35@gmail.com>
2024-05-21 19:35:45 -0400

    Made more code readable and fixed print bug.

--- cipher.py
+++ cipher.py
@@ -9,104 +9,84 @@ import time
 import os
 import sys
 
-"""
-The Cipher gets an integer from the user and shifts the letters in the cipher-data.txt that many characters. 
-Then return the data.
-"""
-def start_cipher():
-    time.sleep(1)
-
-    string_array = []
-    user_input = 0
+def get_user_input():
     valid_input = False
-    
-    print("")
-    print("Cipher Microservice")
-    print("")
-
-    print("The Cipher gets an integer from the user and shifts the letters read from cipher-data.txt that many times.")
-    print("Input 'x' or the word 'exit' to exit the program.")
-
-    print("")
-    print("Input an integer for the cipher:")
-
-    while valid_input is False:
+    while not valid_input:
         user_input = input()
         try:
-            # Check if the input can be convert into an int
             user_input_int = int(user_input)
             valid_input = True
+            return user_input_int
         except ValueError:
-            if user_input == "x":
-                print("Thank you for using the Cipher. Goodbye.")
-                sys.exit(0)
-            elif user_input == "exit":
+            if user_input.lower() in ["x", "exit"]:
                 print("Thank you for using the Cipher. Goodbye.")
                 sys.exit(0)
             else:
                 print("Please type a valid integer.")
 
-    valid_input = False
+"""
+Source: Shift letters by a certain value in python
+URL: https://stackoverflow.com/questions/48514673/shift-letters-by-a-certain-value-in-python 
+Usage: I used this as a reference to create the letter shift.
+"""
+def shift_letters(letter, shift):
+    new_letter = ord(letter) + shift
+    if 'A' <= letter <= 'Z': 
+        return chr((new_letter - ord('A')) % 26 + ord('A'))
+    elif 'a' <= letter <= 'z': 
+        return chr((new_letter - ord('a')) % 26 + ord('a'))
+    else:
+        print("Invalid character in input. Please make sure the input is alphabetic and try again.")
+        sys.exit(0)
 
-    """
-    Source: Shift letters by a certain value in python
-    URL: https://stackoverflow.com/questions/48514673/shift-letters-by-a-certain-value-in-python 
-    Usage: I used this as a reference to create the letter shift.
-    """
-    def shift_letters(letter, shift):
-        
-        new_letter = ord(letter) + shift
-        if 'A' <= letter <= 'Z': 
-            return chr((new_letter - ord('A')) % 26 + ord('A'))
-        elif 'a' <= letter <= 'z': 
-            return chr((new_letter - ord('a')) % 26 + ord('a'))
-        else:
-            print("Invalid character in input. Please make sure the input is alphabetic and try again.")
-            sys.exit(0)
-
-    cipher_num = int(user_input)
-
-    path_dirname = os.path.dirname(__file__)
-    file_path = os.path.join(path_dirname, "cipher-data.txt")
-
+def read_file(file_path):
     with open(file_path, "r") as original_text:
-        file_string = original_text.read()
+        return original_text.read()
 
-    for char in file_string:
-        string_array.append(char)
+def write_file(file_path, data):
+    with open(file_path, "w") as cipher_data:
+        cipher_data.write(data)
 
+def process_cipher(file_string, cipher_num):
+    string_array = list(file_string)
     for i in range(len(string_array)):
         string_array[i] = shift_letters(string_array[i], cipher_num)
-    
-    cipher_result = ''.join(string_array)
-    print("The cipher shifted value is: " + cipher_result)
+    return ''.join(string_array)
 
-    file_path = os.path.join(path_dirname, "cipher-data.txt")
-    with open(file_path, "w") as cipher_data:
-        cipher_data.write(cipher_result)
-    
-    print("The shifted value has been written to cipher-data.txt")
-
-    print("")
-    print("Input a different number? [yes/no]")
-
-    while valid_input is False :
-        user_input = input()
-
-        if user_input == "yes":
-            start_cipher()
-        elif user_input == "no":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
-        elif user_input == "x":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
-        elif user_input == "exit":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
+def handle_user_choice():
+    valid_input = False
+    while not valid_input:
+        user_input = input().lower()
+        if user_input in ["yes", "no", "x", "exit"]:
+            if user_input == "yes":
+                start_cipher()
+            else:
+                print("Thank you for using the Cipher. Goodbye.")
+                sys.exit(0)
         else:
             print("Please type a valid input.")
 
+def start_cipher():
+    time.sleep(1)
+    print("\nCipher Microservice\n")
+    print("The Cipher gets an integer from the user and shifts the letters read fromcipher-input.txt that many times.")
+    print("Input 'x' or the word 'exit' to exit the program.\n")
+    print("Input an integer for the cipher:")
+
+    cipher_num = get_user_input()
+
+    path_dirname = os.path.dirname(__file__)
+    file_path = os.path.join(path_dirname, "cipher-input.txt")
+    file_string = read_file(file_path)
+
+    cipher_result = process_cipher(file_string, cipher_num)
+    print("The cipher shifted value is: " + cipher_result)
+
+    write_file(file_path, cipher_result)
+    print("The shifted value has been written to cipher-input.txt\n")
+    print("Input a different number? [yes/no]")
+
+    handle_user_choice()
 
 # Start the Cipher
 start_cipher()
--- ciphertestapp.py
+++ ciphertestapp.py
@@ -9,93 +9,71 @@ import time
 import os
 import sys
 
-"""
-The Cipher gets an string from the user and writes it to cipher-data.txt. The string will be used by the cipher. 
-"""
-def start_cipher_test():
-    time.sleep(1)
-
-    string_array = []
-    user_input = 0
-    valid_input = False
-    
-    print("")
-    print("Cipher Test App")
-    print("")
-
-    print("This app tests the cipher. It writes user input to cipher-data.txt, which will be used by the cipher.")
-    print("Input the number 0 to exit the program.")
-
-    print("")
-    print("Please input a word or alphabetic character:")
-    
-
-    while valid_input is False:
+def get_user_input():
+    while True:
         user_input = input()
-
         if user_input == "0":
             print("Thank you for using the Cipher Test App. Goodbye.")
             sys.exit(0)
         elif not user_input.isalpha():
-            print("Please only enter alphabetic words or characters.") 
+            print("Please only enter alphabetic words or characters.")
         else:
-            valid_input = True
+            return user_input
 
-    valid_input = False
+def write_to_file(file_path, data):
+    with open(file_path, "w") as cipher_data:
+        cipher_data.write(data)
+
+def read_from_file(file_path):
+    with open(file_path, "r") as cipher_output:
+        return cipher_output.read()
+
+def ask_user_to_continue():
+    while True:
+        user_input = input().lower()
+        if user_input == "yes":
+            return True
+        elif user_input in ["no", "0"]:
+            print("Thank you for using the Cipher. Goodbye.")
+            sys.exit(0)
+        else:
+            print("Please type a valid input.")
+
+def ask_to_see_output(file_path):
+    while True:
+        user_input = input().lower()
+        if user_input == "yes":
+            file_string = read_from_file(file_path)
+            print("The cipher output is: " + file_string)
+            print("")
+            return
+        elif user_input in ["no", "x", "n", "exit"]:
+            print("Thank you for using the Cipher. Goodbye.")
+            sys.exit(0)
+        else:
+            print("Please type a valid input.")
+
+def start_cipher_test():
+    time.sleep(1)
+    print("\nCipher Test App\n")
+    print("This app tests the cipher. It writes user input to cipher-data.txt, which will be used by the cipher.")
+    print("Input the number 0 to exit the program.\n")
+    print("Please input a word or alphabetic character:")
+
+    user_input = get_user_input()
 
     path_dirname = os.path.dirname(__file__)
     file_path = os.path.join(path_dirname, "cipher-data.txt")
 
-    with open(file_path, "w") as cipher_data:
-        cipher_data.write(user_input)
-    
+    write_to_file(file_path, user_input)
     print("The string: " + user_input + " has been written to cipher-data.txt")
 
     print("Input a different alphabetic word or character? [yes/no]")
+    if ask_user_to_continue():
+        start_cipher_test()
 
-    while valid_input is False :
-        user_input = input()
-
-        if user_input == "yes":
-            start_cipher_test()
-        elif user_input == "no":
-            valid_input = True
-        elif user_input == "0":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
-        else:
-            print("Please type a valid input.")
-
-        
-    valid_input = False
-    print("")
-    print("Would you like to see the cipher output? [yes/no]")
-    while valid_input is False :
-        user_input = input()
-
-        if user_input == "yes":
-            file_path = os.path.join(path_dirname, "cipher-data.txt")
-            with open(file_path, "r") as cipher_output:
-                file_string = cipher_output.read()
-            print("The cipher ouput is: " + file_string)
-            print("")
-            valid_input = True
-            
-        elif user_input == "no":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
-        elif user_input == "x":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
-        elif user_input == "n":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
-        elif user_input == "exit":
-            print("Thank you for using the Cipher. Goodbye.")
-            sys.exit(0)
-        else:
-            print("Please type a valid input.")
-
+    print("\nWould you like to see the cipher output? [yes/no]")
+    ask_to_see_output(file_path)
 
 # Start the Cipher Test App
 start_cipher_test()
